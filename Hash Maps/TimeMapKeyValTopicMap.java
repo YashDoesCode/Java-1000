5e8abc7efd5bf1661c4201884ebc6c96ad6f5c03
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-03-02 05:11:05 +0530

    Add TimeMapKeyValTopicMap.java

---
+++ Hash Maps/TimeMapKeyValTopicMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class TimeMapKeyValTopicMap {
+
+    private Map<String, TreeMap<Integer, String>> mp = new HashMap<>();
+
+    public void set(String key, String value, int timestamp) { mp.putIfAbsent(key, new TreeMap<>());
+        mp.get(key).put(timestamp, value);
+    } public String get(String key, int timestamp) { if (!mp.containsKey(key)) return "";
+        Integer k = mp.get(key).floorKey(timestamp);
+        return k == null ? "" : mp.get(key).get(k);
+    };
+}
