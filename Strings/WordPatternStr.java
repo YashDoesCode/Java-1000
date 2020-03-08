f410a6aa27359a14f48cff9e38472130d8a9a35c
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-03-08 07:49:36 +0530

    Add WordPatternStr.java

---
+++ Strings/WordPatternStr.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class WordPatternStr {
+
+    public boolean wordPattern(String pattern, String s) { String[] words = s.split(" ");
+        if (words.length != pattern.length()) return false;
+        Map<Character, String> mp = new HashMap<>();
+        for (int i = 0;
+        i < pattern.length();
+        i++) { char c = pattern.charAt(i);
+            if (mp.containsKey(c)) { if (!mp.get(c).equals(words[i])) return false;
+            } else { if (mp.containsValue(words[i])) return false;
+                mp.put(c, words[i]);
+            } } return true;
+    };
+}
