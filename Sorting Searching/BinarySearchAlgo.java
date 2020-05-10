6eccc99c9864c50800fb7003b07b56c972e3bf26
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-05-11 05:08:05 +0530

    Add BinarySearchAlgo.java

---
+++ Sorting Searching/BinarySearchAlgo.java
@@ -0,0 +1,10 @@
+public class BinarySearchAlgo {
+
+    public int search(int[] a, int target) { int l = 0, r = a.length - 1;
+        while (l <= r) { int mid = l + (r - l) / 2;
+            if (a[mid] == target) return mid;
+            if (a[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        } return -1;
+    };
+}
