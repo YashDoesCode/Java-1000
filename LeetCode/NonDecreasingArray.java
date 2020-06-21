bdcda62c9631931382efff2dffced38d01b0ff41
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-06-22 04:11:29 +0530

    Add NonDecreasingArray.java

---
+++ LeetCode/NonDecreasingArray.java
@@ -0,0 +1,13 @@
+
+public class NonDecreasingArray {
+
+    public boolean checkPossibility(int[] nums) { int cnt = 0;
+        for (int i = 1;
+        i < nums.length;
+        i++) { if (nums[i - 1] > nums[i]) { cnt++;
+                if (cnt > 1) return false;
+                if (i < 2 || nums[i - 2] <= nums[i]) nums[i - 1] = nums[i];
+                else nums[i] = nums[i - 1];
+            } } return true;
+    };
+}
