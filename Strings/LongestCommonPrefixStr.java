99ef84174f21e63def4f1c94c3f2d9030e198590
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-08-12 22:10:17 +0530

    Add LongestCommonPrefixStr.java

---
+++ Strings/LongestCommonPrefixStr.java
@@ -0,0 +1,10 @@
+public class LongestCommonPrefixStr {
+
+    public String lcp(String[] strs) { if (strs == null || strs.length == 0) return "";
+        String p = strs[0];
+        for (int i = 1;
+        i < strs.length;
+        i++) { while (strs[i].indexOf(p) != 0) p = p.substring(0, p.length() - 1);
+        } return p;
+    };
+}
