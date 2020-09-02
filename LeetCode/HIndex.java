051de925e3ca28ba2b7260cc79e8948d13feeb5d
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-09-02 12:31:13 +0530

    Add HIndex.java

---
+++ LeetCode/HIndex.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class HIndex {
+
+    public int hIndex(int[] c) {
+        Arrays.sort(c);
+        int n = c.length;
+        for (int i = 0; i < n; i++) {
+            if (c[i] >= n - i) return n - i;
+        }
+        return 0;
+    }
+}
