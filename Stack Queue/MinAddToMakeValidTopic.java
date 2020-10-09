f6529c983b7757d0a178e6f94afc8ab8adf39116
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-10-09 19:19:23 +0530

    Add MinAddToMakeValidTopic.java

---
+++ Stack Queue/MinAddToMakeValidTopic.java
@@ -0,0 +1,9 @@
+public class MinAddToMakeValidTopic {
+
+    public int minAddToMakeValid(String s) { int left = 0, right = 0;
+        for (char c : s.toCharArray()) { if (c == '(') left++;
+            else { if (left > 0) left--;
+                else right++;
+            } } return left + right;
+    };
+}
