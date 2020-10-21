8d333118f4b2b764e99965ab6b00ff1b9914cb6e
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-10-21 18:14:46 +0530

    Add UniquePaths3DDP.java

---
+++ Arrays 3D/UniquePaths3DDP.java
@@ -0,0 +1,18 @@
+public class UniquePaths3DDP {
+
+    public int uniquePaths3D(int x, int y, int z) { int[][][] dp = new int[x][y][z];
+        for (int i = 0;
+        i < x;
+        i++) for (int j = 0;
+        j < y;
+        j++) for (int k = 0;
+        k < z;
+        k++) { if (i == 0 && j == 0 && k == 0) dp[i][j][k] = 1;
+            else { int val = 0;
+                if (i > 0) val += dp[i - 1][j][k];
+                if (j > 0) val += dp[i][j - 1][k];
+                if (k > 0) val += dp[i][j][k - 1];
+                dp[i][j][k] = val;
+            } } return dp[x - 1][y - 1][z - 1];
+    };
+}
