d688aa00a69f0d3e378dacfb5aef2cb167d937d5
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2020-10-22 15:14:46 +0530

    Add FourSumII.java

---
+++ LeetCode/FourSumII.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class FourSumII {
+
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) { Map<Integer, Integer> mp = new HashMap<>();
+        for (int a : A) for (int b : B) mp.put(a + b, mp.getOrDefault(a + b, 0) + 1);
+        int cnt = 0;
+        for (int c : C) for (int d : D) cnt += mp.getOrDefault(-(c + d), 0);
+        return cnt;
+    };
+}
