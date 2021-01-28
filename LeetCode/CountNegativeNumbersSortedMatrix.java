fa9e237ec3962959e447cc4bfd4aee7250f389ab
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-01-28 05:34:44 +0530

    Add CountNegativeNumbersSortedMatrix.java

---
+++ LeetCode/CountNegativeNumbersSortedMatrix.java
@@ -0,0 +1,10 @@
+
+public class CountNegativeNumbersSortedMatrix {
+
+    public int countNegatives(int[][] grid) { int r = grid.length, c = grid[0].length, cnt = 0, row = r - 1, col = 0;
+        while (row >= 0 && col < c) { if (grid[row][col] < 0) { cnt += c - col;
+                row--;
+            } else col++;
+        } return cnt;
+    };
+}
