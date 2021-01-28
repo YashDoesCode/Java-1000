ff8e0ae7e51bfbdc4a6eb904ea300ec815af1dc2
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-01-28 22:03:25 +0530

    Add LongestArithmeticSubseqTopicDP.java

---
+++ Dynamic Programming/LongestArithmeticSubseqTopicDP.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LongestArithmeticSubseqTopicDP {
+
+    public int longestArithSeqLength(int[] nums) { int n = nums.length, maxV = 0;
+        Map<Integer, Integer>[] dp = new HashMap[n];
+        for (int i = 0;
+        i < n;
+        i++) { dp[i] = new HashMap<>();
+            for (int j = 0;
+            j < i;
+            j++) { int diff = nums[i] - nums[j];
+                int len = dp[j].getOrDefault(diff, 1) + 1;
+                dp[i].put(diff, len);
+                maxV = Math.max(maxV, len);
+            } } return maxV;
+    };
+}
