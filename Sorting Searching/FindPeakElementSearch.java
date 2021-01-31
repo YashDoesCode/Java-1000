d173d539484fe20436c4c448cf7db24d56f885bd
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-01 04:33:43 +0530

    Add FindPeakElementSearch.java

---
+++ Sorting Searching/FindPeakElementSearch.java
@@ -0,0 +1,9 @@
+public class FindPeakElementSearch {
+
+    public int findPeak(int[] a) { int l = 0, r = a.length - 1;
+        while (l < r) { int mid = l + (r - l) / 2;
+            if (a[mid] > a[mid + 1]) r = mid;
+            else l = mid + 1;
+        } return l;
+    };
+}
