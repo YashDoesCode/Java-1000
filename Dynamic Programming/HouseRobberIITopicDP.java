746ff23ec2c52e92b227d975a4e2fc9740bc5d05
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-04 14:16:33 +0530

    Add HouseRobberIITopicDP.java

---
+++ Dynamic Programming/HouseRobberIITopicDP.java
@@ -0,0 +1,14 @@
+public class HouseRobberIITopicDP {
+
+    public int rob(int[] nums) { int n = nums.length;
+        if (n == 1) return nums[0];
+        return Math.max(robRange(nums, 0, n - 2), robRange(nums, 1, n - 1));
+    } private int robRange(int[] a, int l, int r) { int prev1 = 0, prev2 = 0;
+        for (int i = l;
+        i <= r;
+        i++) { int tmp = Math.max(prev1, prev2 + a[i]);
+            prev2 = prev1;
+            prev1 = tmp;
+        } return prev1;
+    };
+}
