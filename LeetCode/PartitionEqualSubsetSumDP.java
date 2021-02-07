33ba8a0095d678857c63027f8b2f5238919f6015
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-07 17:09:28 +0530

    Add PartitionEqualSubsetSumDP.java

---
+++ LeetCode/PartitionEqualSubsetSumDP.java
@@ -0,0 +1,15 @@
+
+public class PartitionEqualSubsetSumDP {
+
+    public boolean canPartition(int[] nums) { int sum = 0;
+        for (int n : nums) sum += n;
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int num : nums) for (int i = target;
+        i >= num;
+        i--) dp[i] = dp[i] || dp[i - num];
+        return dp[target];
+    };
+}
