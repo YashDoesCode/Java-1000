8428800a2480d6ef5cddf6abe5918171a383d41b
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-09 14:43:55 +0530

    Add ValidateStackSequencesStack.java

---
+++ LeetCode/ValidateStackSequencesStack.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class ValidateStackSequencesStack {
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) { Stack<Integer> st = new Stack<>();
+        int j = 0;
+        for (int x : pushed) { st.push(x);
+            while (!st.isEmpty() && j < popped.length && st.peek() == popped[j]) { st.pop();
+                j++;
+            } } return j == popped.length;
+    };
+}
