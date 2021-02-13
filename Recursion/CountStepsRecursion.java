09af9a559ba9cffcfe2b08271f053d8b8a632289
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-13 11:16:08 +0530

    Add CountStepsRecursion.java

---
+++ Recursion/CountStepsRecursion.java
@@ -0,0 +1,6 @@
+public class CountStepsRecursion {
+
+    public int numberOfSteps(int num) { if (num == 0) return 0;
+        return 1 + (num % 2 == 0 ? numberOfSteps(num / 2) : numberOfSteps(num - 1));
+    };
+}
