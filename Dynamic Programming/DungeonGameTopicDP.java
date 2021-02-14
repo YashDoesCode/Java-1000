493f35344709323ec6287303991e8b08e1776d0b
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-14 10:41:01 +0530

    Add DungeonGameTopicDP.java

---
+++ Dynamic Programming/DungeonGameTopicDP.java
@@ -0,0 +1,20 @@
+public class DungeonGameTopicDP {
+
+    public int calculateMinimumHP(int[][] dungeon) { int r = dungeon.length, c = dungeon[0].length;
+        int[][] dp = new int[r][c];
+        dp[r - 1][c - 1] = Math.max(1, 1 - dungeon[r - 1][c - 1]);
+        for (int i = r - 2;
+        i >= 0;
+        i--) dp[i][c - 1] = Math.max(1, dp[i + 1][c - 1] - dungeon[i][c - 1]);
+        for (int j = c - 2;
+        j >= 0;
+        j--) dp[r - 1][j] = Math.max(1, dp[r - 1][j + 1] - dungeon[r - 1][j]);
+        for (int i = r - 2;
+        i >= 0;
+        i--) for (int j = c - 2;
+        j >= 0;
+        j--) { int minNext = Math.min(dp[i + 1][j], dp[i][j + 1]);
+            dp[i][j] = Math.max(1, minNext - dungeon[i][j]);
+        } return dp[0][0];
+    };
+}
