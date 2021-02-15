e82f23fe2f7f9f7a876aecba337cbc6963b92337
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-15 12:04:16 +0530

    Add ValidateStackSequencesTopic.java

---
+++ Stack Queue/ValidateStackSequencesTopic.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ValidateStackSequencesTopic {
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) { Stack<Integer> st = new Stack<>();
+        int j = 0;
+        for (int x : pushed) { st.push(x);
+            while (!st.isEmpty() && j < popped.length && st.peek() == popped[j]) { st.pop();
+                j++;
+            } } return j == popped.length;
+    };
+}
