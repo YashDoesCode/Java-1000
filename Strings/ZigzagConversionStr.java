71c32ab76e76413440600ce33c25ecd6289c1e77
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-16 08:33:46 +0530

    Add ZigzagConversionStr.java

---
+++ Strings/ZigzagConversionStr.java
@@ -0,0 +1,17 @@
+public class ZigzagConversionStr {
+
+    public String convert(String s, int numRows) { if (numRows == 1) return s;
+        StringBuilder[] sbs = new StringBuilder[numRows];
+        for (int i = 0;
+        i < numRows;
+        i++) sbs[i] = new StringBuilder();
+        int idx = 0, dir = 1;
+        for (char c : s.toCharArray()) { sbs[idx].append(c);
+            if (idx == 0) dir = 1;
+            else if (idx == numRows - 1) dir = -1;
+            idx += dir;
+        } StringBuilder res = new StringBuilder();
+        for (StringBuilder sb : sbs) res.append(sb);
+        return res.toString();
+    };
+}
