ebf6be51cce9a569a2fab9d3a7c1c567c67d61e4
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-16 23:21:00 +0530

    Add BinaryGapBit.java

---
+++ LeetCode/BinaryGapBit.java
@@ -0,0 +1,11 @@
+
+public class BinaryGapBit {
+
+    public int binaryGap(int n) { int last = -1, ans = 0;
+        for (int i = 0;
+        i < 32;
+        i++) { if (((n >> i) & 1) == 1) { if (last != -1) ans = Math.max(ans, i - last);
+                last = i;
+            } } return ans;
+    };
+}
