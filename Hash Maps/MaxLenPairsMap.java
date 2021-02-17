438ba691aebf1a9310b959a7cfff985c51916461
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-17 16:04:47 +0530

    Add MaxLenPairsMap.java

---
+++ Hash Maps/MaxLenPairsMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MaxLenPairsMap {
+
+    public int findLongestChain(int[][] pairs) { Arrays.sort(pairs, (a, b) -> Integer.compare(a[1], b[1]));
+        int cur = Integer.MIN_VALUE, cnt = 0;
+        for (int[] p : pairs) if (p[0] > cur) { cur = p[1];
+            cnt++;
+        } return cnt;
+    };
+}
