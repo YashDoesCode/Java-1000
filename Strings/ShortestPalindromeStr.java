23f85980f15b43be7659b94f9fbdc644ed5b4347
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-02-22 13:20:11 +0530

    Add ShortestPalindromeStr.java

---
+++ Strings/ShortestPalindromeStr.java
@@ -0,0 +1,16 @@
+public class ShortestPalindromeStr {
+
+    public String shortestPalindrome(String s) { String temp = s + "#" + new StringBuilder(s).reverse().toString();
+        int[] table = getTable(temp);
+        return new StringBuilder(s.substring(table[table.length - 1])).reverse().toString() + s;
+    } private int[] getTable(String s) { int[] table = new int[s.length()];
+        int index = 0;
+        for (int i = 1;
+        i < s.length();
+        i++) { if (s.charAt(i) == s.charAt(index)) table[i] = ++index;
+            else { if (index != 0) { index = table[index - 1];
+                    i--;
+                } else table[i] = 0;
+            } } return table;
+    };
+}
