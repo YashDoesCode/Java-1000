b324c335524fbc129ad599c4046cecf568540f07
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-02 03:17:22 +0530

    Add HashMapBasicOperations.java

---
+++ Hash Maps/HashMapBasicOperations.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class HashMapBasicOperations {
+
+    public Map<String, Integer> createMap() { Map<String, Integer> mp = new HashMap<>();
+        mp.put("A", 1);
+        mp.put("B", 2);
+        return mp;
+    };
+}
