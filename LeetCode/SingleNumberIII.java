44d595c7ccf3fd3f26847a238cc19ff15785c762
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-06 11:22:03 +0530

    Add SingleNumberIII.java

---
+++ LeetCode/SingleNumberIII.java
@@ -0,0 +1,12 @@
+
+public class SingleNumberIII {
+
+    public int[] singleNumber(int[] nums) { int diff = 0;
+        for (int n : nums) diff ^= n;
+        diff &= -diff;
+        int[] res = new int[2];
+        for (int n : nums) { if ((n & diff) == 0) res[0] ^= n;
+            else res[1] ^= n;
+        } return res;
+    };
+}
