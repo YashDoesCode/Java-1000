1f7ed4f01a776699a9483498c54e996614b69f14
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-11 05:43:08 +0530

    Add FindEventualSafeStatesGraph.java

---
+++ LeetCode/FindEventualSafeStatesGraph.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class FindEventualSafeStatesGraph {
+
+    public List<Integer> eventualSafeNodes(int[][] graph) { int n = graph.length;
+        int[] state = new int[n];
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0;
+        i < n;
+        i++) if (dfs(graph, i, state)) res.add(i);
+        return res;
+    } private boolean dfs(int[][] g, int u, int[] state) { if (state[u] > 0) return state[u] == 2;
+        state[u] = 1;
+        for (int v : g[u]) if (!dfs(g, v, state)) return false;
+        state[u] = 2;
+        return true;
+    };
+}
