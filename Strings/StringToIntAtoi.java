93b3e786b0181e7c6518d0a931d2f1bda741676f
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-16 16:06:37 +0530

    Add StringToIntAtoi.java

---
+++ Strings/StringToIntAtoi.java
@@ -0,0 +1,15 @@
+public class StringToIntAtoi {
+
+    public int atoi(String s) { s = s.trim();
+        if (s.isEmpty()) return 0;
+        int idx = 0, sign = 1;
+        long res = 0;
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') { sign = (s.charAt(0) == '-') ? -1 : 1;
+            idx++;
+        } while (idx < s.length() && Character.isDigit(s.charAt(idx))) { res = res * 10 + (s.charAt(idx) - '0');
+            if (res * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (res * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            idx++;
+        } return (int) (res * sign);
+    };
+}
