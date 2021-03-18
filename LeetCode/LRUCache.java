a8b8f3c9d64d7f95e3d0657695207528b3d4633d
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-19 03:25:51 +0530

    Add LRUCache.java

---
+++ LeetCode/LRUCache.java
@@ -0,0 +1,133 @@
+/*
+ * Popular Question: Least Recently Used (LRU) Cache Design
+ * Source: LeetCode #146 (Medium / Top Interview Classic)
+ *
+ * Problem Statement:
+ * Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+ *
+ * Implement the LRUCache class:
+ * - LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+ * - int get(int key) Return the value of the key if the key exists, otherwise return -1.
+ * - void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair
+ *   to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+ *
+ * The functions get and put must each run in O(1) average time complexity.
+ *
+ * Example 1:
+ * Input:
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * Output:
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ *
+ * Constraints:
+ * 1 <= capacity <= 3000
+ * 0 <= key <= 10^4
+ * 0 <= value <= 10^5
+ * At most 2 * 10^5 calls will be made to get and put.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    private static class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node() {}
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final Map<Integer, Node> map = new HashMap<>();
+    private final int capacity;
+    private int size;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.head = new Node();
+        this.tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        Node node = map.get(key);
+        if (node == null) {
+            return -1;
+        }
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        Node node = map.get(key);
+
+        if (node != null) {
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            addNode(newNode);
+            size++;
+
+            if (size > capacity) {
+                Node tailPrev = popTail();
+                map.remove(tailPrev.key);
+                size--;
+            }
+        }
+    }
+
+    private void addNode(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    private Node popTail() {
+        Node res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LRUCache lru = new LRUCache(2);
+
+        lru.put(1, 1);
+        lru.put(2, 2);
+        System.out.println("get(1): " + lru.get(1));
+
+        lru.put(3, 3);
+        System.out.println("get(2): " + lru.get(2));
+
+        lru.put(4, 4);
+        System.out.println("get(1): " + lru.get(1));
+        System.out.println("get(3): " + lru.get(3));
+        System.out.println("get(4): " + lru.get(4));
+    }
+}
