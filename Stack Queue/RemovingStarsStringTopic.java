d30943abb2a8288e701f127859c2dff09d395c64
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-18 15:10:37 +0530

    Add RemovingStarsStringTopic.java

---
+++ Stack Queue/RemovingStarsStringTopic.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class RemovingStarsStringTopic {
+
+    public String removeStars(String s) { StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) { if (c == '*') sb.deleteCharAt(sb.length() - 1);
+            else sb.append(c);
+        } return sb.toString();
+    };
+}
