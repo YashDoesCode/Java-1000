6cb404362143435618b2c35be09a536fda5ed400
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-24 00:53:06 +0530

    Add RowAndMaxOnes2D.java

---
+++ Arrays 2D/RowAndMaxOnes2D.java
@@ -0,0 +1,14 @@
+public class RowAndMaxOnes2D {
+
+    public int[] rowAndMaximumOnes(int[][] mat) { int r = mat.length, c = mat[0].length, maxR = 0, maxCnt = -1;
+        for (int i = 0;
+        i < r;
+        i++) { int cnt = 0;
+            for (int j = 0;
+            j < c;
+            j++) cnt += mat[i][j];
+            if (cnt > maxCnt) { maxCnt = cnt;
+                maxR = i;
+            } } return new int[]{maxR, maxCnt};
+    };
+}
