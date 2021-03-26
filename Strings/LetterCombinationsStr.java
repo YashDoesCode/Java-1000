37fc80ff35e1a0f1eae76a5fa694817e413d4d2c
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-27 03:43:45 +0530

    Add LetterCombinationsStr.java

---
+++ Strings/LetterCombinationsStr.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class LetterCombinationsStr {
+
+    private String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) { List<String> res = new ArrayList<>();
+        if (digits.isEmpty()) return res;
+        dfs(digits, 0, "", res);
+        return res;
+    } private void dfs(String d, int idx, String cur, List<String> res) { if (idx == d.length()) { res.add(cur);
+            return;
+        } String letters = map[d.charAt(idx) - '0'];
+        for (char c : letters.toCharArray()) dfs(d, idx + 1, cur + c, res);
+    };
+}
