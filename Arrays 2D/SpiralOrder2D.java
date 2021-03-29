5888d9cee6e4eb36bc7a5391016f6d4d57e605c1
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-29 20:24:37 +0530

    Add SpiralOrder2D.java

---
+++ Arrays 2D/SpiralOrder2D.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SpiralOrder2D {
+
+    public List<Integer> spiral(int[][] m) { List<Integer> res = new ArrayList<>();
+        int r1 = 0, r2 = m.length - 1, c1 = 0, c2 = m[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) { for (int c = c1;
+            c <= c2;
+            c++) res.add(m[r1][c]);
+            for (int r = r1 + 1;
+            r <= r2;
+            r++) res.add(m[r][c2]);
+            if (r1 < r2 && c1 < c2) { for (int c = c2 - 1;
+                c > c1;
+                c--) res.add(m[r2][c]);
+                for (int r = r2;
+                r > r1;
+                r--) res.add(m[r][c1]);
+            } r1++;
+            r2--;
+            c1++;
+            c2--;
+        } return res;
+    };
+}
