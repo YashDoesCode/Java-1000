01902eb5520877ec51bee6ee661050ded8e495da
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-03-31 19:51:12 +0530

    Add ContainsDuplicateII.java

---
+++ LeetCode/ContainsDuplicateII.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class ContainsDuplicateII {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) { Set<Integer> s = new HashSet<>();
+        for (int i = 0;
+        i < nums.length;
+        i++) { if (i > k) s.remove(nums[i - k - 1]);
+            if (!s.add(nums[i])) return true;
+        } return false;
+    };
+}
