d5c81a48f35d1bb2dc84156c94c1d731aad59f94
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-04-03 13:25:13 +0530

    Add FastInverseSquareRoot.java

---
+++ Math And Numbers/FastInverseSquareRoot.java
@@ -0,0 +1,10 @@
+public class FastInverseSquareRoot {
+
+    public float invSqrt(float x) { float xhalf = 0.5f * x;
+        int i = Float.floatToIntBits(x);
+        i = 0x5f3759df - (i >> 1);
+        x = Float.intBitsToFloat(i);
+        x = x * (1.5f - xhalf * x * x);
+        return x;
+    };
+}
