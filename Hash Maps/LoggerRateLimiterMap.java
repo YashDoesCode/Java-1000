eda3fc11e3f8f3a2b0823c613fc76851b45d4cc4
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-04-12 00:38:10 +0530

    Add LoggerRateLimiterMap.java

---
+++ Hash Maps/LoggerRateLimiterMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class LoggerRateLimiterMap {
+
+    private Map<String, Integer> mp = new HashMap<>();
+
+    public boolean shouldPrintMessage(int timestamp, String message) { if (!mp.containsKey(message) || timestamp - mp.get(message) >= 10) { mp.put(message, timestamp);
+            return true;
+        } return false;
+    };
+}
