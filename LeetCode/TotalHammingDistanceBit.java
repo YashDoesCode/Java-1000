ff918a906218082196fdb8d6071b4be5396d02a2
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-04-16 02:04:42 +0530

    Add TotalHammingDistanceBit.java

---
+++ LeetCode/TotalHammingDistanceBit.java
@@ -0,0 +1,12 @@
+
+public class TotalHammingDistanceBit {
+
+    public int totalHammingDistance(int[] nums) { int ans = 0, n = nums.length;
+        for (int i = 0;
+        i < 32;
+        i++) { int count = 0;
+            for (int num : nums) count += (num >> i) & 1;
+            ans += count * (n - count);
+        } return ans;
+    };
+}
