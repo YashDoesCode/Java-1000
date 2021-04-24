a9561082a874510492422d954f7712a13f469577
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-04-24 15:14:36 +0530

    Add FlippingAnImage.java

---
+++ LeetCode/FlippingAnImage.java
@@ -0,0 +1,12 @@
+
+public class FlippingAnImage {
+
+    public int[][] flipAndInvertImage(int[][] A) { int c = A[0].length;
+        for (int[] row : A) for (int i = 0;
+        i < (c + 1) / 2;
+        i++) { int t = row[i] ^ 1;
+            row[i] = row[c - 1 - i] ^ 1;
+            row[c - 1 - i] = t;
+        } return A;
+    };
+}
