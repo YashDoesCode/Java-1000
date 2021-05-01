b7b9895cceca71ee441c2f0a6250f1703a729668
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-02 01:54:01 +0530

    Add MinStackTopic.java

---
+++ Stack Queue/MinStackTopic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MinStackTopic {
+
+    private Stack<Integer> st = new Stack<>(), minSt = new Stack<>();
+
+    public void push(int val) { st.push(val);
+        if (minSt.isEmpty() || val <= minSt.peek()) minSt.push(val);
+    } public void pop() { if (st.peek().equals(minSt.peek())) minSt.pop();
+        st.pop();
+    } public int top() { return st.peek();
+    } public int getMin() { return minSt.peek();
+    };
+}
