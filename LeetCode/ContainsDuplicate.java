a427fc63c5749d0587e0d92856c92b179baa7d23
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-06 05:56:40 +0530

    Add ContainsDuplicate.java

---
+++ LeetCode/ContainsDuplicate.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) { Set<Integer> s = new HashSet<>();
+        for (int x : nums) if (!s.add(x)) return true;
+        return false;
+    };
+}
