0269d758bd97d4bd1c2d39565a1162bbaceb8599
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-06 14:15:42 +0530

    Add SqrtXMath.java

---
+++ LeetCode/SqrtXMath.java
@@ -0,0 +1,12 @@
+
+public class SqrtXMath {
+
+    public int mySqrt(int x) { if (x == 0) return 0;
+        int l = 1, r = x, ans = 0;
+        while (l <= r) { int mid = l + (r - l) / 2;
+            if (mid <= x / mid) { ans = mid;
+                l = mid + 1;
+            } else r = mid - 1;
+        } return ans;
+    };
+}
