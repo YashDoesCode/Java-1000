c4376048ea1c78e35dcc4ba2e91a4ce18fb40cb5
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-07 12:23:54 +0530

    Add LongestCommonPrefix.java

---
+++ LeetCode/LongestCommonPrefix.java
@@ -0,0 +1,11 @@
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) { if (strs == null || strs.length == 0) return "";
+        String pref = strs[0];
+        for (int i = 1;
+        i < strs.length;
+        i++) { while (strs[i].indexOf(pref) != 0) pref = pref.substring(0, pref.length() - 1);
+        } return pref;
+    };
+}
