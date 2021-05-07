97055192aaaf5ce1e146be70f5d7bf08aa7e2e07
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-07 16:43:16 +0530

    Add SortArrayByParity.java

---
+++ LeetCode/SortArrayByParity.java
@@ -0,0 +1,12 @@
+
+public class SortArrayByParity {
+
+    public int[] sortArrayByParity(int[] nums) { int l = 0, r = nums.length - 1;
+        while (l < r) { if (nums[l] % 2 > nums[r] % 2) { int t = nums[l];
+                nums[l] = nums[r];
+                nums[r] = t;
+            } if (nums[l] % 2 == 0) l++;
+            if (nums[r] % 2 == 1) r--;
+        } return nums;
+    };
+}
