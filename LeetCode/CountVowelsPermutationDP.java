751e2ad29c8015e309dc8a57207fa3d73470754f
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-11 12:01:43 +0530

    Add CountVowelsPermutationDP.java

---
+++ LeetCode/CountVowelsPermutationDP.java
@@ -0,0 +1,20 @@
+
+public class CountVowelsPermutationDP {
+
+    public int countVowelsPermutation(int n) { long MOD = 1000000007;
+        long a = 1, e = 1, i = 1, o = 1, u = 1;
+        for (int k = 1;
+        k < n;
+        k++) { long aNext = (e + i + u) % MOD;
+            long eNext = (a + i) % MOD;
+            long iNext = (e + o) % MOD;
+            long oNext = i;
+            long uNext = (i + o) % MOD;
+            a = aNext;
+            e = eNext;
+            i = iNext;
+            o = oNext;
+            u = uNext;
+        } return (int) ((a + e + i + o + u) % MOD);
+    };
+}
