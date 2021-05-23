39a08b5aa59706d461a367d1e002fc0a06fb618d
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-23 20:53:07 +0530

    Add UniqueNumberOfOccurrencesMap.java

---
+++ LeetCode/UniqueNumberOfOccurrencesMap.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class UniqueNumberOfOccurrencesMap {
+
+    public boolean uniqueOccurrences(int[] arr) { Map<Integer, Integer> mp = new HashMap<>();
+        for (int n : arr) mp.put(n, mp.getOrDefault(n, 0) + 1);
+        Set<Integer> s = new HashSet<>(mp.values());
+        return s.size() == mp.size();
+    };
+}
