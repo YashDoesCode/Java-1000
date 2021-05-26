92b030f37236bb57bfa2265cf31bb585b0c444f5
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-26 13:06:58 +0530

    Add EqualOccurrencesCheckMap.java

---
+++ Hash Maps/EqualOccurrencesCheckMap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class EqualOccurrencesCheckMap {
+
+    public boolean areOccurrencesEqual(String s) { int[] cnt = new int[26];
+        for (char c : s.toCharArray()) cnt[c - 'a']++;
+        int val = 0;
+        for (int c : cnt) if (c > 0) { if (val == 0) val = c;
+            else if (val != c) return false;
+        } return true;
+    };
+}
