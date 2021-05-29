d9745bea2163ba57a5746467e57eb5c8b6298e6a
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-05-30 01:07:50 +0530

    Add MinimumFallingPathSumDP.java

---
+++ LeetCode/MinimumFallingPathSumDP.java
@@ -0,0 +1,19 @@
+
+public class MinimumFallingPathSumDP {
+
+    public int minFallingPathSum(int[][] matrix) { int n = matrix.length;
+        for (int i = 1;
+        i < n;
+        i++) for (int j = 0;
+        j < n;
+        j++) { int best = matrix[i - 1][j];
+            if (j > 0) best = Math.min(best, matrix[i - 1][j - 1]);
+            if (j < n - 1) best = Math.min(best, matrix[i - 1][j + 1]);
+            matrix[i][j] += best;
+        } int ans = matrix[n - 1][0];
+        for (int j = 1;
+        j < n;
+        j++) ans = Math.min(ans, matrix[n - 1][j]);
+        return ans;
+    };
+}
