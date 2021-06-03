5586a6fb6d2ecae99da4ed6a5d6b3d6bc8fcac4f
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-06-03 12:56:51 +0530

    Add MinRemoveValidParenthesesTopic.java

---
+++ Stack Queue/MinRemoveValidParenthesesTopic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MinRemoveValidParenthesesTopic {
+
+    public String minRemoveToMakeValid(String s) { StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0;
+        i < sb.length();
+        i++) { if (sb.charAt(i) == '(') st.push(i);
+            else if (sb.charAt(i) == ')') { if (!st.isEmpty()) st.pop();
+                else sb.setCharAt(i, '*');
+            } } while (!st.isEmpty()) sb.setCharAt(st.pop(), '*');
+        return sb.toString().replace("*", "");
+    };
+}
