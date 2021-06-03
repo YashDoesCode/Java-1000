986537cdeb61cb0ffe763f1e6f4f2cae1406e050
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-06-03 11:04:40 +0530

    Add HIndexSearch.java

---
+++ Sorting Searching/HIndexSearch.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HIndexSearch {
+
+    public int hIndex(int[] c) { Arrays.sort(c);
+        int n = c.length;
+        for (int i = 0;
+        i < n;
+        i++) if (c[i] >= n - i) return n - i;
+        return 0;
+    };
+}
