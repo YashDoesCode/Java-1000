fdad2f49bb1be75a75a0c6fd8a78cc91b34737fd
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-06-04 04:13:56 +0530

    Add MaxLayerSum3DGrid.java

---
+++ Arrays 3D/MaxLayerSum3DGrid.java
@@ -0,0 +1,15 @@
+public class MaxLayerSum3DGrid {
+
+    public int maxLayerSum(int[][][] grid) { int x = grid.length, maxV = Integer.MIN_VALUE;
+        for (int i = 0;
+        i < x;
+        i++) { int sum = 0;
+            for (int j = 0;
+            j < grid[0].length;
+            j++) for (int k = 0;
+            k < grid[0][0].length;
+            k++) sum += grid[i][j][k];
+            maxV = Math.max(maxV, sum);
+        } return maxV;
+    };
+}
