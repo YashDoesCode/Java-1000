e34c0c34d230dcef9c40c3f2b5e6b631aa3830ad
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-06-09 15:59:01 +0530

    Add ReverseLinkedList.java

---
+++ LeetCode/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+
+public class ReverseLinkedList {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode() {} ListNode(int val) { this.val = val;
+        } ListNode(int val, ListNode next) { this.val = val;
+            this.next = next;
+        };
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, cur = head;
+        while (cur != null) { ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        } return prev;
+    };
+}
