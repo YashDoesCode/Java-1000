ed82e85aeb380fe32ba764992cc660630ed4ca36
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-06-09 08:19:44 +0530

    Add ModularExponentiationMath.java

---
+++ Math And Numbers/ModularExponentiationMath.java
@@ -0,0 +1,10 @@
+public class ModularExponentiationMath {
+
+    public long power(long base, long exp, long mod) { long res = 1;
+        base %= mod;
+        while (exp > 0) { if (exp % 2 == 1) res = (res * base) % mod;
+            base = (base * base) % mod;
+            exp /= 2;
+        } return res;
+    };
+}
