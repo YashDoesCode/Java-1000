f1746d1b18f68bb07f3327f68e644f903385f451
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-06-22 00:18:55 +0530

    Add DesignFrontMiddleBackQueue.java

---
+++ LeetCode/DesignFrontMiddleBackQueue.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class DesignFrontMiddleBackQueue {
+
+    private LinkedList<Integer> list = new LinkedList<>();
+
+    public void pushFront(int val) { list.addFirst(val);
+    } public void pushMiddle(int val) { list.add(list.size() / 2, val);
+    } public void pushBack(int val) { list.addLast(val);
+    } public int popFront() { return list.isEmpty() ? -1 : list.removeFirst();
+    } public int popMiddle() { return list.isEmpty() ? -1 : list.remove((list.size() - 1) / 2);
+    } public int popBack() { return list.isEmpty() ? -1 : list.removeLast();
+    };
+}
