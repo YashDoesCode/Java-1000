793a28ed7a92283589f0fd602c6585ba3c6f2f49
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-06-30 03:53:11 +0530

    Add SortMatrixDiagonally2D.java

---
+++ Arrays 2D/SortMatrixDiagonally2D.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class SortMatrixDiagonally2D {
+
+    public int[][] diagonalSort(int[][] mat) { int m = mat.length, n = mat[0].length;
+        Map<Integer, PriorityQueue<Integer>> d = new HashMap<>();
+        for (int i = 0;
+        i < m;
+        i++) for (int j = 0;
+        j < n;
+        j++) { d.putIfAbsent(i - j, new PriorityQueue<>());
+            d.get(i - j).add(mat[i][j]);
+        } for (int i = 0;
+        i < m;
+        i++) for (int j = 0;
+        j < n;
+        j++) mat[i][j] = d.get(i - j).poll();
+        return mat;
+    };
+}
