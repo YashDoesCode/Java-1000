ad740c7f769906fa5aa87d5c210c12679ff187ba
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-01 02:40:40 +0530

    Add RansomNoteMap.java

---
+++ LeetCode/RansomNoteMap.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class RansomNoteMap {
+
+    public boolean canConstruct(String ransomNote, String magazine) { int[] cnt = new int[26];
+        for (char c : magazine.toCharArray()) cnt[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) { if (--cnt[c - 'a'] < 0) return false;
+        } return true;
+    };
+}
