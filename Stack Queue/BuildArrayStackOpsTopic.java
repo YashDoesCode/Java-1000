46f4fa8605ebc1c65426bec17f591f3c03f1c7b1
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-04 21:52:05 +0530

    Add BuildArrayStackOpsTopic.java

---
+++ Stack Queue/BuildArrayStackOpsTopic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class BuildArrayStackOpsTopic {
+
+    public List<String> buildArray(int[] target, int n) { List<String> res = new ArrayList<>();
+        int cur = 1;
+        for (int num : target) { while (cur < num) { res.add("Push");
+                res.add("Pop");
+                cur++;
+            } res.add("Push");
+            cur++;
+        } return res;
+    };
+}
