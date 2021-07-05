d83ffeac7dd57ca16f5cd73c82b0eedc0339b0f9
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-06 03:45:02 +0530

    Add MinimumAddToMakeParenthesesValid.java

---
+++ LeetCode/MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,10 @@
+
+public class MinimumAddToMakeParenthesesValid {
+
+    public int minAddToMakeValid(String s) { int left = 0, right = 0;
+        for (char c : s.toCharArray()) { if (c == '(') left++;
+            else { if (left > 0) left--;
+                else right++;
+            } } return left + right;
+    };
+}
