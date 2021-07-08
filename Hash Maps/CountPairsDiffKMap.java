4634a3ddc687c25aeaea9e6e38057fee2f607ba8
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-08 22:40:41 +0530

    Add CountPairsDiffKMap.java

---
+++ Hash Maps/CountPairsDiffKMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CountPairsDiffKMap {
+
+    public int findPairs(int[] nums, int k) { if (k < 0) return 0;
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int n : nums) mp.put(n, mp.getOrDefault(n, 0) + 1);
+        int cnt = 0;
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) { if (k == 0) { if (entry.getValue() >= 2) cnt++;
+            } else if (mp.containsKey(entry.getKey() + k)) cnt++;
+        } return cnt;
+    };
+}
