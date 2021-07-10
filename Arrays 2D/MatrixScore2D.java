5f1c1a922d77791d8bec0d31393104e3e0ece274
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-10 10:46:09 +0530

    Add MatrixScore2D.java

---
+++ Arrays 2D/MatrixScore2D.java
@@ -0,0 +1,14 @@
+public class MatrixScore2D {
+
+    public int matrixScore(int[][] grid) { int m = grid.length, n = grid[0].length;
+        int res = (1 << (n - 1)) * m;
+        for (int j = 1;
+        j < n;
+        j++) { int cur = 0;
+            for (int i = 0;
+            i < m;
+            i++) cur += grid[i][j] ^ grid[i][0];
+            res += Math.max(cur, m - cur) * (1 << (n - 1 - j));
+        } return res;
+    };
+}
