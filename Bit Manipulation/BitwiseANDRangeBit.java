83a21f4fcc0b20efe9760e524f948bd69751cebe
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-10 12:50:22 +0530

    Add BitwiseANDRangeBit.java

---
+++ Bit Manipulation/BitwiseANDRangeBit.java
@@ -0,0 +1,9 @@
+public class BitwiseANDRangeBit {
+
+    public int rangeBitwiseAnd(int left, int right) { int shift = 0;
+        while (left < right) { left >>= 1;
+            right >>= 1;
+            shift++;
+        } return left << shift;
+    };
+}
