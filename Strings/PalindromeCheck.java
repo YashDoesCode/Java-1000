f4eae859e0fffdf5cd3ba55531865d2a588d862c
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-14 02:11:01 +0530

    Add PalindromeCheck.java

---
+++ Strings/PalindromeCheck.java
@@ -0,0 +1,7 @@
+public class PalindromeCheck {
+
+    public boolean isPalindrome(String s) { int l = 0, r = s.length() - 1;
+        while (l < r) if (s.charAt(l++) != s.charAt(r--)) return false;
+        return true;
+    };
+}
