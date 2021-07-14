fee07c717e639b06306fe2b266fe56c9de4ce659
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-14 21:29:20 +0530

    Add GrayCodeBit.java

---
+++ LeetCode/GrayCodeBit.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class GrayCodeBit {
+
+    public List<Integer> grayCode(int n) { List<Integer> res = new ArrayList<>();
+        for (int i = 0;
+        i < (1 << n);
+        i++) res.add(i ^ (i >> 1));
+        return res;
+    };
+}
