ff2ca4b1e9d7eb4e91d63f19b04caa94d63f76ac
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-15 09:26:05 +0530

    Add SortArrayByIncreasingFrequencyTopic.java

---
+++ Sorting Searching/SortArrayByIncreasingFrequencyTopic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SortArrayByIncreasingFrequencyTopic {
+
+    public int[] frequencySort(int[] nums) { Map<Integer, Integer> mp = new HashMap<>();
+        for (int n : nums) mp.put(n, mp.getOrDefault(n, 0) + 1);
+        Integer[] a = new Integer[nums.length];
+        for (int i = 0;
+        i < nums.length;
+        i++) a[i] = nums[i];
+        Arrays.sort(a, (x, y) -> !mp.get(x).equals(mp.get(y)) ? Integer.compare(mp.get(x), mp.get(y)) : Integer.compare(y, x));
+        for (int i = 0;
+        i < nums.length;
+        i++) nums[i] = a[i];
+        return nums;
+    };
+}
