d0f9ad95da562f4c040deea834409f6f4a30f370
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-15 23:06:26 +0530

    Add SubtreeOfAnotherTreeTree.java

---
+++ LeetCode/SubtreeOfAnotherTreeTree.java
@@ -0,0 +1,23 @@
+
+public class SubtreeOfAnotherTreeTree {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {} TreeNode(int val) { this.val = val;
+        } TreeNode(int val, TreeNode left, TreeNode right) { this.val = val;
+            this.left = left;
+            this.right = right;
+        };
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (isSame(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    } private boolean isSame(TreeNode a, TreeNode b) { if (a == null || b == null) return a == b;
+        return a.val == b.val && isSame(a.left, b.left) && isSame(a.right, b.right);
+    };
+}
