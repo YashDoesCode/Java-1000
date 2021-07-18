96b871987929a7495e58d956b4a5dca3bb87b96e
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-18 21:54:15 +0530

    Add DiagonalTraverse.java

---
+++ LeetCode/DiagonalTraverse.java
@@ -0,0 +1,20 @@
+
+public class DiagonalTraverse {
+
+    public int[] findDiagonalOrder(int[][] m) { if (m.length == 0) return new int[0];
+        int r = m.length, c = m[0].length, row = 0, col = 0;
+        int[] res = new int[r * c];
+        for (int i = 0;
+        i < r * c;
+        i++) { res[i] = m[row][col];
+            if ((row + col) % 2 == 0) { if (col == c - 1) row++;
+                else if (row == 0) col++;
+                else { row--;
+                    col++;
+                } } else { if (row == r - 1) col++;
+                else if (col == 0) row++;
+                else { row++;
+                    col--;
+                } } } return res;
+    };
+}
