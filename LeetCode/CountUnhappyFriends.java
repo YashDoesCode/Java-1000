2fe58260d25a95db4384dd7437e700c3f9262829
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-20 03:19:03 +0530

    Add CountUnhappyFriends.java

---
+++ LeetCode/CountUnhappyFriends.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class CountUnhappyFriends {
+
+    public int unhappyFriends(int n, int[][] preferences, int[][] pairs) { int[][] rank = new int[n][n];
+        for (int i = 0;
+        i < n;
+        i++) for (int j = 0;
+        j < n - 1;
+        j++) rank[i][preferences[i][j]] = j;
+        int[] match = new int[n];
+        for (int[] p : pairs) { match[p[0]] = p[1];
+            match[p[1]] = p[0];
+        } int ans = 0;
+        for (int x = 0;
+        x < n;
+        x++) { int y = match[x];
+            int r = rank[x][y];
+            for (int i = 0;
+            i < r;
+            i++) { int u = preferences[x][i];
+                int v = match[u];
+                if (rank[u][x] < rank[u][v]) { ans++;
+                    break;
+                } } } return ans;
+    };
+}
