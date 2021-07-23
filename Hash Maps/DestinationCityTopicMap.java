adc8002e4a77cdacce93a77c3246424b8282cea5
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-07-23 20:43:39 +0530

    Add DestinationCityTopicMap.java

---
+++ Hash Maps/DestinationCityTopicMap.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class DestinationCityTopicMap {
+
+    public String destCity(List<List<String>> paths) { Set<String> src = new HashSet<>();
+        for (List<String> p : paths) src.add(p.get(0));
+        for (List<String> p : paths) if (!src.contains(p.get(1))) return p.get(1);
+        return "";
+    };
+}
