b556e7dc7f4ffb1be7edc874826c7acfce499b6d
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-08-12 14:43:19 +0530

    Add SingleNumberIIBit.java

---
+++ Bit Manipulation/SingleNumberIIBit.java
@@ -0,0 +1,8 @@
+public class SingleNumberIIBit {
+
+    public int singleNumber(int[] nums) { int ones = 0, twos = 0;
+        for (int n : nums) { ones = (ones ^ n) & ~twos;
+            twos = (twos ^ n) & ~ones;
+        } return ones;
+    };
+}
