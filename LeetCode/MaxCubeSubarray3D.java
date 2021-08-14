dff7ad3683338495398f6c2265638400efd20af8
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-08-14 14:11:26 +0530

    Add MaxCubeSubarray3D.java

---
+++ LeetCode/MaxCubeSubarray3D.java
@@ -0,0 +1,14 @@
+
+public class MaxCubeSubarray3D {
+
+    public int maxCubeSum(int[][][] g) { int x = g.length, maxV = g[0][0][0];
+        for (int i = 0;
+        i < x;
+        i++) for (int j = 0;
+        j < g[0].length;
+        j++) for (int k = 0;
+        k < g[0][0].length;
+        k++) maxV = Math.max(maxV, g[i][j][k]);
+        return maxV;
+    };
+}
