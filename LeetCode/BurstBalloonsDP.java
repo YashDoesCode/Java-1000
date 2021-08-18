29ed961a06e8e6d3ab1022ccade20d1ef5fb3045
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-08-18 12:45:42 +0530

    Add BurstBalloonsDP.java

---
+++ LeetCode/BurstBalloonsDP.java
@@ -0,0 +1,21 @@
+
+public class BurstBalloonsDP {
+
+    public int maxCoins(int[] nums) { int n = nums.length;
+        int[] a = new int[n + 2];
+        a[0] = a[n + 1] = 1;
+        for (int i = 0;
+        i < n;
+        i++) a[i + 1] = nums[i];
+        int[][] dp = new int[n + 2][n + 2];
+        for (int k = 1;
+        k <= n;
+        k++) { for (int left = 1;
+            left <= n - k + 1;
+            left++) { int right = left + k - 1;
+                for (int i = left;
+                i <= right;
+                i++) { dp[left][right] = Math.max(dp[left][right], a[left - 1] * a[i] * a[right + 1] + dp[left][i - 1] + dp[i + 1][right]);
+                } } } return dp[1][n];
+    };
+}
