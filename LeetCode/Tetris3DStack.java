3132d4023f9d4c21a0dba18fa3a9c27c2b744fea
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-08-18 16:06:11 +0530

    Add Tetris3DStack.java

---
+++ LeetCode/Tetris3DStack.java
@@ -0,0 +1,13 @@
+
+public class Tetris3DStack {
+
+    public boolean canPlace3D(int[][][] grid, int x, int y, int z, int dx, int dy, int dz) { for (int i = x;
+        i < x + dx;
+        i++) for (int j = y;
+        j < y + dy;
+        j++) for (int k = z;
+        k < z + dz;
+        k++) if (i >= grid.length || j >= grid[0].length || k >= grid[0][0].length || grid[i][j][k] != 0) return false;
+        return true;
+    };
+}
