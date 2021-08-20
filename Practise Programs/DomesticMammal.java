ec6829c1840ef9f39e924ba0106582ae2fc1a950
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-08-20 19:04:54 +0530

    Add DomesticMammal.java

---
+++ Practise Programs/DomesticMammal.java
@@ -0,0 +1,13 @@
+
+package PractisePrograms;
+
+class DomesticMammal {
+
+    DomesticMammal() {
+        System.out.println("default domestic mammal.");
+    }
+
+    void display() {
+        System.out.println("of domestic mammal.");
+    }
+}
