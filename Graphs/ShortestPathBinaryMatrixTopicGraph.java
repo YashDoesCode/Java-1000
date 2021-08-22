0e3869cc8dd48469e799acb53d7b33c56824db73
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-08-22 14:59:45 +0530

    Add ShortestPathBinaryMatrixTopicGraph.java

---
+++ Graphs/ShortestPathBinaryMatrixTopicGraph.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ShortestPathBinaryMatrixTopicGraph {
+
+    public int shortestPathBinaryMatrix(int[][] grid) { int n = grid.length;
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) return -1;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 1});
+        grid[0][0] = 1;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        while (!q.isEmpty()) { int[] cur = q.poll();
+            int r = cur[0], c = cur[1], d = cur[2];
+            if (r == n - 1 && c == n - 1) return d;
+            for (int[] dir : dirs) { int nr = r + dir[0], nc = c + dir[1];
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0) { grid[nr][nc] = 1;
+                    q.add(new int[]{nr, nc, d + 1});
+                } } } return -1;
+    };
+}
