2f3b6e516d09386101ade94f0816b973ceaef1a7
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-08-26 06:39:24 +0530

    Add MinDeletionsUniqueFreqMap.java

---
+++ Hash Maps/MinDeletionsUniqueFreqMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MinDeletionsUniqueFreqMap {
+
+    public int minDeletions(String s) { int[] cnt = new int[26];
+        for (char c : s.toCharArray()) cnt[c - 'a']++;
+        Set<Integer> used = new HashSet<>();
+        int del = 0;
+        for (int f : cnt) while (f > 0 && !used.add(f)) { f--;
+            del++;
+        } return del;
+    };
+}
