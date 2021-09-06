c4aa67d902c15257131d6f314a2dbfd961e86825
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-09-06 08:27:41 +0530

    Add QuickSortAlgo.java

---
+++ Sorting Searching/QuickSortAlgo.java
@@ -0,0 +1,18 @@
+public class QuickSortAlgo {
+
+    public void sort(int[] a, int l, int r) { if (l < r) { int p = partition(a, l, r);
+            sort(a, l, p - 1);
+            sort(a, p + 1, r);
+        } } private int partition(int[] a, int l, int r) { int pivot = a[r], i = l - 1;
+        for (int j = l;
+        j < r;
+        j++) if (a[j] < pivot) { i++;
+            int t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        } int t = a[i + 1];
+        a[i + 1] = a[r];
+        a[r] = t;
+        return i + 1;
+    };
+}
