de7797fab6daf97c0d7b15a73102bcf01b514ad9
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-09-07 10:20:42 +0530

    Add SubsetsBitmask.java

---
+++ LeetCode/SubsetsBitmask.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class SubsetsBitmask {
+
+    public List<List<Integer>> subsets(int[] nums) { List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0;
+        i < (1 << n);
+        i++) { List<Integer> cur = new ArrayList<>();
+            for (int j = 0;
+            j < n;
+            j++) if ((i & (1 << j)) != 0) cur.add(nums[j]);
+            res.add(cur);
+        } return res;
+    };
+}
