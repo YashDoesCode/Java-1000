5d941ca810e00c995bf02f0f5d6e36856743df5a
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-09-13 04:04:42 +0530

    Add BipartiteGraphCheck.java

---
+++ Graphs/BipartiteGraphCheck.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class BipartiteGraphCheck {
+
+    public boolean isBipartite(int[][] graph) { int n = graph.length;
+        int[] color = new int[n];
+        for (int i = 0;
+        i < n;
+        i++) { if (color[i] == 0) { Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                color[i] = 1;
+                while (!q.isEmpty()) { int u = q.poll();
+                    for (int v : graph[u]) { if (color[v] == 0) { color[v] = -color[u];
+                            q.add(v);
+                        } else if (color[v] == color[u]) return false;
+                    } } } } return true;
+    };
+}
