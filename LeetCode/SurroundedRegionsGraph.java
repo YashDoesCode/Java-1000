fbca9e47604a6b20b07c80ac0c865679b647cef2
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-09-30 08:22:30 +0530

    Add SurroundedRegionsGraph.java

---
+++ LeetCode/SurroundedRegionsGraph.java
@@ -0,0 +1,26 @@
+
+public class SurroundedRegionsGraph {
+
+    public void solve(char[][] board) { int r = board.length, c = board[0].length;
+        for (int i = 0;
+        i < r;
+        i++) { dfs(board, i, 0);
+            dfs(board, i, c - 1);
+        } for (int j = 0;
+        j < c;
+        j++) { dfs(board, 0, j);
+            dfs(board, r - 1, j);
+        } for (int i = 0;
+        i < r;
+        i++) for (int j = 0;
+        j < c;
+        j++) { if (board[i][j] == 'O') board[i][j] = 'X';
+            else if (board[i][j] == 'E') board[i][j] = 'O';
+        } } private void dfs(char[][] b, int r, int c) { if (r < 0 || c < 0 || r >= b.length || c >= b[0].length || b[r][c] != 'O') return;
+        b[r][c] = 'E';
+        dfs(b, r + 1, c);
+        dfs(b, r - 1, c);
+        dfs(b, r, c + 1);
+        dfs(b, r, c - 1);
+    };
+}
