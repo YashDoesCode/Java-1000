acc936950c0d5d4288339df11db3eaa76de1e982
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-09-30 14:13:21 +0530

    Add KthLargestElementSearch.java

---
+++ Sorting Searching/KthLargestElementSearch.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class KthLargestElementSearch {
+
+    public int findKthLargest(int[] nums, int k) { PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int x : nums) { pq.add(x);
+            if (pq.size() > k) pq.poll();
+        } return pq.peek();
+    };
+}
