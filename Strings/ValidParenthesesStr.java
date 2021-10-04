a1ebe5502e3b03f193cc47efb49afee64e2abfd8
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-05 04:37:53 +0530

    Add ValidParenthesesStr.java

---
+++ Strings/ValidParenthesesStr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ValidParenthesesStr {
+
+    public boolean isValid(String s) { Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) { if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else if (st.isEmpty() || st.pop() != c) return false;
+        } return st.isEmpty();
+    };
+}
