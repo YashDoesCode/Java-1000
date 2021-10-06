481bd67b06d2c06db97eb0f981872cfa5913d406
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-06 16:17:49 +0530

    Add UpperBoundSearch.java

---
+++ Sorting Searching/UpperBoundSearch.java
@@ -0,0 +1,9 @@
+public class UpperBoundSearch {
+
+    public int upperBound(int[] a, int target) { int l = 0, r = a.length;
+        while (l < r) { int mid = l + (r - l) / 2;
+            if (a[mid] > target) r = mid;
+            else l = mid + 1;
+        } return l;
+    };
+}
