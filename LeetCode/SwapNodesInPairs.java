bc8d195acf58d82c7a8b3981c1e67f59d038390b
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-09 10:19:41 +0530

    Add SwapNodesInPairs.java

---
+++ LeetCode/SwapNodesInPairs.java
@@ -0,0 +1,21 @@
+
+public class SwapNodesInPairs {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode() {} ListNode(int val) { this.val = val;
+        } ListNode(int val, ListNode next) { this.val = val;
+            this.next = next;
+        };
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    };
+}
