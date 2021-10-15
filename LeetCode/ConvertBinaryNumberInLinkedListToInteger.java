646638898ae7c136f8c66c6df9c7c88c4ab14604
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-15 19:18:19 +0530

    Add ConvertBinaryNumberInLinkedListToInteger.java

---
+++ LeetCode/ConvertBinaryNumberInLinkedListToInteger.java
@@ -0,0 +1,20 @@
+
+public class ConvertBinaryNumberInLinkedListToInteger {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode() {} ListNode(int val) { this.val = val;
+        } ListNode(int val, ListNode next) { this.val = val;
+            this.next = next;
+        };
+    }
+
+    public int getDecimalValue(ListNode head) {
+        int ans = 0;
+        while (head != null) { ans = (ans << 1) | head.val;
+            head = head.next;
+        } return ans;
+    };
+}
