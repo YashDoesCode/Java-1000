a314c547e7188388e1725a6524612ee27d2b5be7
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-16 14:22:53 +0530

    Add FlyodsTriangle.java

---
+++ Practise Programs/FlyodsTriangle.java
@@ -0,0 +1,28 @@
+
+package PractisePrograms;
+
+import java.util.Scanner;
+
+public class FlyodsTriangle {
+
+    public FlyodsTriangle() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter number of rows :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+
+        for(int i = 1; i <= n; ++i) {
+            for(int j = 1; j <= i; ++j) {
+                System.out.print("" + count + " ");
+                ++count;
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
