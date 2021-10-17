312f5c74dcfeadee448c506847eed5568aacddde
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-18 02:45:12 +0530

    Add CellsWithOddValuesMatrix.java

---
+++ LeetCode/CellsWithOddValuesMatrix.java
@@ -0,0 +1,15 @@
+
+public class CellsWithOddValuesMatrix {
+
+    public int oddCells(int m, int n, int[][] indices) { int[] r = new int[m], c = new int[n];
+        for (int[] idx : indices) { r[idx[0]]++;
+            c[idx[1]]++;
+        } int cnt = 0;
+        for (int i = 0;
+        i < m;
+        i++) for (int j = 0;
+        j < n;
+        j++) if ((r[i] + c[j]) % 2 == 1) cnt++;
+        return cnt;
+    };
+}
