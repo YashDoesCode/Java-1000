18a338657918191d36df2e800b4375605d769566
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-17 14:45:42 +0530

    Add SubsetsIIDupRecursion.java

---
+++ Recursion/SubsetsIIDupRecursion.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SubsetsIIDupRecursion {
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) { Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, 0, new ArrayList<>(), res);
+        return res;
+    } private void dfs(int[] nums, int start, List<Integer> cur, List<List<Integer>> res) { res.add(new ArrayList<>(cur));
+        for (int i = start;
+        i < nums.length;
+        i++) { if (i > start && nums[i] == nums[i - 1]) continue;
+            cur.add(nums[i]);
+            dfs(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        } };
+}
