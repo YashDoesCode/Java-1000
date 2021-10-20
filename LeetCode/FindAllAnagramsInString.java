11aef952b84dda92f94441b6d9c8891bfaa2c0c7
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-20 22:29:52 +0530

    Add FindAllAnagramsInString.java

---
+++ LeetCode/FindAllAnagramsInString.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class FindAllAnagramsInString {
+
+    public List<Integer> findAnagrams(String s, String p) { List<Integer> res = new ArrayList<>();
+        if (s.length() < p.length()) return res;
+        int[] pCnt = new int[26], sCnt = new int[26];
+        for (char c : p.toCharArray()) pCnt[c - 'a']++;
+        for (int i = 0;
+        i < s.length();
+        i++) { sCnt[s.charAt(i) - 'a']++;
+            if (i >= p.length()) sCnt[s.charAt(i - p.length()) - 'a']--;
+            if (Arrays.equals(pCnt, sCnt)) res.add(i - p.length() + 1);
+        } return res;
+    };
+}
