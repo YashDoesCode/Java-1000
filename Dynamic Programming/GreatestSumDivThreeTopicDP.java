8df4662c2f56c358e7f9ed0313a9a4e5c5ae6fb9
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-21 11:14:13 +0530

    Add GreatestSumDivThreeTopicDP.java

---
+++ Dynamic Programming/GreatestSumDivThreeTopicDP.java
@@ -0,0 +1,12 @@
+public class GreatestSumDivThreeTopicDP {
+
+    public int maxSumDivThree(int[] nums) { int[] dp = new int[]{0, Integer.MIN_VALUE, Integer.MIN_VALUE};
+        for (int x : nums) { int[] next = dp.clone();
+            for (int i = 0;
+            i < 3;
+            i++) if (dp[i] != Integer.MIN_VALUE) { int rem = (i + x) % 3;
+                next[rem] = Math.max(next[rem], dp[i] + x);
+            } dp = next;
+        } return dp[0];
+    };
+}
