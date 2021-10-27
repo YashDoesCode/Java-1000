2591c995bb47ad205632a5feab6cd8cc5cb60b4c
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-27 19:55:39 +0530

    Add KthDistinctStringInArray.java

---
+++ LeetCode/KthDistinctStringInArray.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class KthDistinctStringInArray {
+
+    public String kthDistinct(String[] arr, int k) { Map<String, Integer> mp = new LinkedHashMap<>();
+        for (String s : arr) mp.put(s, mp.getOrDefault(s, 0) + 1);
+        for (String s : mp.keySet()) { if (mp.get(s) == 1) { k--;
+                if (k == 0) return s;
+            } } return "";
+    };
+}
