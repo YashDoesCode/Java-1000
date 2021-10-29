663fc62cde949629e9bfe77b3a997f6cc860cfda
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-10-30 02:31:16 +0530

    Add CatalanNumberMath.java

---
+++ Math And Numbers/CatalanNumberMath.java
@@ -0,0 +1,12 @@
+public class CatalanNumberMath {
+
+    public long catalan(int n) { long[] c = new long[n + 1];
+        c[0] = 1;
+        for (int i = 1;
+        i <= n;
+        i++) for (int j = 0;
+        j < i;
+        j++) c[i] += c[j] * c[i - 1 - j];
+        return c[n];
+    };
+}
