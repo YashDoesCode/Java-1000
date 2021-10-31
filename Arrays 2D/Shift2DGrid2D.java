7019dbd234b0e163982d40efba13de760968fe84
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-01 04:48:46 +0530

    Add Shift2DGrid2D.java

---
+++ Arrays 2D/Shift2DGrid2D.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Shift2DGrid2D {
+
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) { int m = grid.length, n = grid[0].length, total = m * n;
+        k %= total;
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0;
+        i < m;
+        i++) { List<Integer> row = new ArrayList<>();
+            for (int j = 0;
+            j < n;
+            j++) { int idx = (i * n + j - k + total) % total;
+                row.add(grid[idx / n][idx % n]);
+            } res.add(row);
+        } return res;
+    };
+}
