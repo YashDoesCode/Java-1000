e2c5a6ae54e17638d6a5db85b30d3fa0849beb68
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-06 19:03:15 +0530

    Add DecodingXORedArrayBit.java

---
+++ LeetCode/DecodingXORedArrayBit.java
@@ -0,0 +1,12 @@
+
+public class DecodingXORedArrayBit {
+
+    public int[] decode(int[] encoded, int first) { int n = encoded.length + 1;
+        int[] ans = new int[n];
+        ans[0] = first;
+        for (int i = 0;
+        i < encoded.length;
+        i++) ans[i + 1] = ans[i] ^ encoded[i];
+        return ans;
+    };
+}
