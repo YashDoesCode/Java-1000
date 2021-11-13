77da5103ed7f56947ac7d5a1f6421575627eb3cf
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-13 07:48:39 +0530

    Add TernarySearchEngine.java

---
+++ Sorting Searching/TernarySearchEngine.java
@@ -0,0 +1,19 @@
+public class TernarySearchEngine {
+
+    public int ternarySearch(int[] arr, int key) {
+        int l = 0, r = arr.length - 1;
+        while (r >= l) {
+            int mid1 = l + (r - l) / 3;
+            int mid2 = r - (r - l) / 3;
+            if (arr[mid1] == key) return mid1;
+            if (arr[mid2] == key) return mid2;
+            if (key < arr[mid1]) r = mid1 - 1;
+            else if (key > arr[mid2]) l = mid2 + 1;
+            else {
+                l = mid1 + 1;
+                r = mid2 - 1;
+            }
+        }
+        return -1;
+    }
+}
