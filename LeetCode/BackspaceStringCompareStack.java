696b7f881acd344a3210a175939f6b3f6d96e732
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-14 23:11:45 +0530

    Add BackspaceStringCompareStack.java

---
+++ LeetCode/BackspaceStringCompareStack.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class BackspaceStringCompareStack {
+
+    public boolean backspaceCompare(String s, String t) { return build(s).equals(build(t));
+    } private String build(String str) { Stack<Character> st = new Stack<>();
+        for (char c : str.toCharArray()) { if (c != '#') st.push(c);
+            else if (!st.isEmpty()) st.pop();
+        } return String.valueOf(st);
+    };
+}
