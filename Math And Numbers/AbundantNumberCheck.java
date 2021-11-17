21995fe040b98f6bf8e1f3a1295c5f81f1802186
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-17 12:14:29 +0530

    Add AbundantNumberCheck.java

---
+++ Math And Numbers/AbundantNumberCheck.java
@@ -0,0 +1,9 @@
+public class AbundantNumberCheck {
+
+    public boolean isAbundant(int n) { int sum = 0;
+        for (int i = 1;
+        i <= n / 2;
+        i++) if (n % i == 0) sum += i;
+        return sum > n;
+    };
+}
