7d54972e9a3141d18d1379eeb3cc2435ffa5e6e2
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-19 09:03:43 +0530

    Add RemoveAdjacentDuplicatesIITopic.java

---
+++ Stack Queue/RemoveAdjacentDuplicatesIITopic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class RemoveAdjacentDuplicatesIITopic {
+
+    public String removeDuplicates(String s, int k) { Stack<int[]> st = new Stack<>();
+        for (char c : s.toCharArray()) { if (!st.isEmpty() && st.peek()[0] == c) { st.peek()[1]++;
+                if (st.peek()[1] == k) st.pop();
+            } else st.push(new int[]{c, 1});
+        } StringBuilder sb = new StringBuilder();
+        for (int[] p : st) for (int i = 0;
+        i < p[1];
+        i++) sb.append((char) p[0]);
+        return sb.toString();
+    };
+}
