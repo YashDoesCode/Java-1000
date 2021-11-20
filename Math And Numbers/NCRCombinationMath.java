a849a57075c74c0dbc3ccd897f01f01a8623835a
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-20 21:01:53 +0530

    Add NCRCombinationMath.java

---
+++ Math And Numbers/NCRCombinationMath.java
@@ -0,0 +1,11 @@
+public class NCRCombinationMath {
+
+    public long nCr(int n, int r) { if (r > n) return 0;
+        if (r == 0 || r == n) return 1;
+        long res = 1;
+        for (int i = 1;
+        i <= r;
+        i++) res = res * (n - i + 1) / i;
+        return res;
+    };
+}
