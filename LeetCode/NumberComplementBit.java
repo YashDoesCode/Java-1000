e92cb2f4ad9583295e1b60321019dbec178c9f6b
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-20 11:41:30 +0530

    Add NumberComplementBit.java

---
+++ LeetCode/NumberComplementBit.java
@@ -0,0 +1,8 @@
+
+public class NumberComplementBit {
+
+    public int findComplement(int num) { int mask = ~0;
+        while ((num & mask) != 0) mask <<= 1;
+        return ~mask & ~num;
+    };
+}
