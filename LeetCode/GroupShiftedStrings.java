30d706d11ebec52bcd4c68983fcf394e498e5036
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-23 01:08:07 +0530

    Add GroupShiftedStrings.java

---
+++ LeetCode/GroupShiftedStrings.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class GroupShiftedStrings {
+
+    public List<List<String>> groupStrings(String[] strings) { Map<String, List<String>> mp = new HashMap<>();
+        for (String s : strings) { String key = getKey(s);
+            mp.putIfAbsent(key, new ArrayList<>());
+            mp.get(key).add(s);
+        } return new ArrayList<>(mp.values());
+    } private String getKey(String s) { StringBuilder sb = new StringBuilder();
+        for (int i = 1;
+        i < s.length();
+        i++) { int diff = (s.charAt(i) - s.charAt(i - 1) + 26) % 26;
+            sb.append(diff).append("#");
+        } return sb.toString();
+    };
+}
