c9139ce1980926242b7421ecc253b36309fa5e31
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-27 23:05:52 +0530

    Add RedundantConnectionTopicGraph.java

---
+++ Graphs/RedundantConnectionTopicGraph.java
@@ -0,0 +1,13 @@
+public class RedundantConnectionTopicGraph {
+
+    public int[] findRedundantConnection(int[][] edges) { int[] parent = new int[edges.length + 1];
+        for (int i = 0;
+        i < parent.length;
+        i++) parent[i] = i;
+        for (int[] e : edges) { int root1 = find(parent, e[0]), root2 = find(parent, e[1]);
+            if (root1 == root2) return e;
+            parent[root1] = root2;
+        } return new int[0];
+    } private int find(int[] p, int i) { return p[i] == i ? i : (p[i] = find(p, p[i]));
+    };
+}
