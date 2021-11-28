4eec1a8d09c4c758be05885a900dd763eba062cc
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-29 03:30:40 +0530

    Add PatternPrinterPyramid.java

---
+++ Practise Programs/PatternPrinterPyramid.java
@@ -0,0 +1,13 @@
+public class PatternPrinterPyramid {
+
+    public void printPyramid(int n) { for (int i = 1;
+        i <= n;
+        i++) { for (int j = i;
+            j < n;
+            j++) System.out.print(" ");
+            for (int k = 1;
+            k <= (2 * i - 1);
+            k++) System.out.print("*");
+            System.out.println();
+        } };
+}
