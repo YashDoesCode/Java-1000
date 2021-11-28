2d7c8270e38edcc5ffb2380c714d2e3a1932d3e0
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-11-29 02:55:00 +0530

    Add PerfectNumberCheck.java

---
+++ Math And Numbers/PerfectNumberCheck.java
@@ -0,0 +1,11 @@
+public class PerfectNumberCheck {
+
+    public boolean isPerfect(int n) { if (n <= 1) return false;
+        int sum = 1;
+        for (int i = 2;
+        i * i <= n;
+        i++) if (n % i == 0) { sum += i;
+            if (i * i != n) sum += n / i;
+        } return sum == n;
+    };
+}
