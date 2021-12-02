201dd11582d8eb6a7687e9ee5ebd61cd0af1d23f
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-12-02 13:21:32 +0530

    Add PalindromeRecursion.java

---
+++ Recursion/PalindromeRecursion.java
@@ -0,0 +1,6 @@
+public class PalindromeRecursion {
+
+    public boolean isPal(String s, int l, int r) { if (l >= r) return true;
+        return s.charAt(l) == s.charAt(r) && isPal(s, l + 1, r - 1);
+    };
+}
