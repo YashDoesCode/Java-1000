4e937c54036cb8e59e1a83a89e6dc73ca8e60526
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-12-11 05:16:14 +0530

    Add PartitionArrayMaximumSumTopicDP.java

---
+++ Dynamic Programming/PartitionArrayMaximumSumTopicDP.java
@@ -0,0 +1,14 @@
+public class PartitionArrayMaximumSumTopicDP {
+
+    public int maxSumAfterPartitioning(int[] arr, int k) { int n = arr.length;
+        int[] dp = new int[n + 1];
+        for (int i = 1;
+        i <= n;
+        i++) { int maxV = 0;
+            for (int j = 1;
+            j <= k && i - j >= 0;
+            j++) { maxV = Math.max(maxV, arr[i - j]);
+                dp[i] = Math.max(dp[i], dp[i - j] + maxV * j);
+            } } return dp[n];
+    };
+}
