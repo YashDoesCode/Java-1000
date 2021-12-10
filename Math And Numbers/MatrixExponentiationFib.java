43e23933ccfb81f6c97ab4f56006d35aef4d1dc6
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-12-10 08:25:54 +0530

    Add MatrixExponentiationFib.java

---
+++ Math And Numbers/MatrixExponentiationFib.java
@@ -0,0 +1,21 @@
+public class MatrixExponentiationFib {
+
+    public int fib(int n) { if (n <= 1) return n;
+        int[][] F = {{1, 1}, {1, 0}};
+        power(F, n - 1);
+        return F[0][0];
+    } private void power(int[][] F, int n) { if (n == 0 || n == 1) return;
+        int[][] M = {{1, 1}, {1, 0}};
+        power(F, n / 2);
+        multiply(F, F);
+        if (n % 2 != 0) multiply(F, M);
+    } private void multiply(int[][] F, int[][] M) { int x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+        int y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+        int z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+        int w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
+        F[0][0] = x;
+        F[0][1] = y;
+        F[1][0] = z;
+        F[1][1] = w;
+    };
+}
