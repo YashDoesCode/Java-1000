8813f927dae6d90f74e0b7ac0a1a9ce4a66e9850
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-12-14 04:42:29 +0530

    Add EqualRowColumnPairs2D.java

---
+++ Arrays 2D/EqualRowColumnPairs2D.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class EqualRowColumnPairs2D {
+
+    public int equalPairs(int[][] grid) { int n = grid.length, cnt = 0;
+        Map<String, Integer> mp = new HashMap<>();
+        for (int[] row : grid) { String s = Arrays.toString(row);
+            mp.put(s, mp.getOrDefault(s, 0) + 1);
+        } for (int c = 0;
+        c < n;
+        c++) { int[] col = new int[n];
+            for (int r = 0;
+            r < n;
+            r++) col[r] = grid[r][c];
+            cnt += mp.getOrDefault(Arrays.toString(col), 0);
+        } return cnt;
+    };
+}
