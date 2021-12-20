a16225c61b5b0eeaceaf942118cc3225140441df
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-12-20 05:39:16 +0530

    Add TrieImplementationTopic.java

---
+++ Trees/TrieImplementationTopic.java
@@ -0,0 +1,19 @@
+public class TrieImplementationTopic {
+
+    class Node { Node[] child = new Node[26];
+
+        boolean isEnd = false;
+    } private Node root = new Node();
+
+    public void insert(String word) { Node cur = root;
+        for (char c : word.toCharArray()) { int idx = c - 'a';
+            if (cur.child[idx] == null) cur.child[idx] = new Node();
+            cur = cur.child[idx];
+        } cur.isEnd = true;
+    } public boolean search(String word) { Node cur = root;
+        for (char c : word.toCharArray()) { int idx = c - 'a';
+            if (cur.child[idx] == null) return false;
+            cur = cur.child[idx];
+        } return cur.isEnd;
+    };
+}
