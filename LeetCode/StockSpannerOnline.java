faefaf3a03f0b5c4468914eea052071ec4812674
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2021-12-26 09:28:22 +0530

    Add StockSpannerOnline.java

---
+++ LeetCode/StockSpannerOnline.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class StockSpannerOnline {
+
+    private Stack<int[]> st = new Stack<>();
+
+    public int next(int price) { int w = 1;
+        while (!st.isEmpty() && st.peek()[0] <= price) w += st.pop()[1];
+        st.push(new int[]{price, w});
+        return w;
+    };
+}
