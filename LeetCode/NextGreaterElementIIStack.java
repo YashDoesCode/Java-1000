11f0863ef7b0875d2eb32fa5bc674b2386e07857
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2022-03-23 16:24:45 +0530

    Add NextGreaterElementIIStack.java

---
+++ LeetCode/NextGreaterElementIIStack.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class NextGreaterElementIIStack {
+
+    public int[] nextGreaterElements(int[] nums) { int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0;
+        i < n * 2;
+        i++) { while (!st.isEmpty() && nums[st.peek()] < nums[i % n]) res[st.pop()] = nums[i % n];
+            if (i < n) st.push(i);
+        } return res;
+    };
+}
