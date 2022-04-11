126daa672bc00ae2300c2ea7556035bf65589a01
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2022-04-11 11:55:56 +0530

    Add FindTheTownJudgeGraph.java

---
+++ LeetCode/FindTheTownJudgeGraph.java
@@ -0,0 +1,12 @@
+
+public class FindTheTownJudgeGraph {
+
+    public int findJudge(int n, int[][] trust) { int[] cnt = new int[n + 1];
+        for (int[] t : trust) { cnt[t[0]]--;
+            cnt[t[1]]++;
+        } for (int i = 1;
+        i <= n;
+        i++) if (cnt[i] == n - 1) return i;
+        return -1;
+    };
+}
