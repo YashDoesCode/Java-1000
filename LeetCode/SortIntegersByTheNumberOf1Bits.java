8a0e0c5c2c7c1e31cdf6e0619e17e0a3cfd8943e
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2022-04-30 23:02:02 +0530

    Add SortIntegersByTheNumberOf1Bits.java

---
+++ LeetCode/SortIntegersByTheNumberOf1Bits.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class SortIntegersByTheNumberOf1Bits {
+
+    public int[] sortByBits(int[] arr) { Integer[] a = new Integer[arr.length];
+        for (int i = 0;
+        i < arr.length;
+        i++) a[i] = arr[i];
+        Arrays.sort(a, (x, y) -> { int c1 = Integer.bitCount(x), c2 = Integer.bitCount(y);
+            return c1 != c2 ? Integer.compare(c1, c2) : Integer.compare(x, y);
+        });
+        for (int i = 0;
+        i < arr.length;
+        i++) arr[i] = a[i];
+        return arr;
+    };
+}
