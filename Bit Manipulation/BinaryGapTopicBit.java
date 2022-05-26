71b0580bd866c8235af5e2499defc2480b67c122
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2022-05-26 08:26:38 +0530

    Add BinaryGapTopicBit.java

---
+++ Bit Manipulation/BinaryGapTopicBit.java
@@ -0,0 +1,10 @@
+public class BinaryGapTopicBit {
+
+    public int binaryGap(int n) { int last = -1, ans = 0;
+        for (int i = 0;
+        i < 32;
+        i++) if (((n >> i) & 1) == 1) { if (last != -1) ans = Math.max(ans, i - last);
+            last = i;
+        } return ans;
+    };
+}
