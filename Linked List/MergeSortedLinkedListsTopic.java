85ac2325b8dc720956ba1dc6ae9306dc79a25881
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2022-09-02 20:50:03 +0530

    Add MergeSortedLinkedListsTopic.java

---
+++ Linked List/MergeSortedLinkedListsTopic.java
@@ -0,0 +1,14 @@
+public class MergeSortedLinkedListsTopic {
+
+    class Node { int val;
+
+        Node next;
+        Node(int v) { val = v;
+        } } public Node merge(Node l1, Node l2) { if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) { l1.next = merge(l1.next, l2);
+            return l1;
+        } else { l2.next = merge(l1, l2.next);
+            return l2;
+        } };
+}
