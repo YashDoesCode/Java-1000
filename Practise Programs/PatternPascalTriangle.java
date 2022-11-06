b49306700fde9b3e3ded1c780540074aa7ad83e9
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2022-11-06 16:34:51 +0530

    Add PatternPascalTriangle.java

---
+++ Practise Programs/PatternPascalTriangle.java
@@ -0,0 +1,12 @@
+public class PatternPascalTriangle {
+
+    public void printPascal(int n) { for (int i = 0;
+        i < n;
+        i++) { int num = 1;
+            for (int j = 0;
+            j <= i;
+            j++) { System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            } System.out.println();
+        } };
+}
