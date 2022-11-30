63c740df4a019eb9a2ac7d000ffa1778eb85a76b
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2022-12-01 03:35:47 +0530

    Add RotateArrayLeft.java

---
+++ Arrays 1D/RotateArrayLeft.java
@@ -0,0 +1,11 @@
+public class RotateArrayLeft {
+
+    public void rotate(int[] a, int k) { k %= a.length;
+        reverse(a, 0, k - 1);
+        reverse(a, k, a.length - 1);
+        reverse(a, 0, a.length - 1);
+    } private void reverse(int[] a, int l, int r) { while (l < r) { int t = a[l];
+            a[l++] = a[r];
+            a[r--] = t;
+        } };
+}
