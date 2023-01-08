b0641fbf6c872426674fcfb04025095a3151bfbb
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-01-08 21:32:22 +0530

    Add SingleNumberII.java

---
+++ LeetCode/SingleNumberII.java
@@ -0,0 +1,9 @@
+
+public class SingleNumberII {
+
+    public int singleNumber(int[] nums) { int ones = 0, twos = 0;
+        for (int n : nums) { ones = (ones ^ n) & ~twos;
+            twos = (twos ^ n) & ~ones;
+        } return ones;
+    };
+}
