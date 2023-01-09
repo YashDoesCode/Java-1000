a530a7f51d3da1e85b217ec1a4374d5d26eb9305
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-01-10 00:56:08 +0530

    Add Sha256Hasher.java

---
+++ Security/SHA 256 Text Hasher/Sha256Hasher.java
@@ -0,0 +1,20 @@
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+public class Sha256Hasher {
+
+    public String hash(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedhash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
