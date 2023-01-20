94440b736c979f78de38dea8ae653ad8ebc479d8
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-01-20 16:34:41 +0530

    Add BitwiseXORAllPairingsTopicBit.java

---
+++ Bit Manipulation/BitwiseXORAllPairingsTopicBit.java
@@ -0,0 +1,8 @@
+public class BitwiseXORAllPairingsTopicBit {
+
+    public int xorAllNums(int[] nums1, int[] nums2) { int ans = 0;
+        if (nums2.length % 2 == 1) for (int n : nums1) ans ^= n;
+        if (nums1.length % 2 == 1) for (int n : nums2) ans ^= n;
+        return ans;
+    };
+}
