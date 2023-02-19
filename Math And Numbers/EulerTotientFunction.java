960c25e58218a62c2d78ede32b70ee3c0fef35e1
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-02-19 05:49:26 +0530

    Add EulerTotientFunction.java

---
+++ Math And Numbers/EulerTotientFunction.java
@@ -0,0 +1,11 @@
+public class EulerTotientFunction {
+
+    public int phi(int n) { int result = n;
+        for (int p = 2;
+        p * p <= n;
+        p++) { if (n % p == 0) { while (n % p == 0) n /= p;
+                result -= result / p;
+            } } if (n > 1) result -= result / n;
+        return result;
+    };
+}
