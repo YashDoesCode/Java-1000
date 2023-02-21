54fb9b455e9b12be4f9de14eb388ea64ca4ad9be
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-02-21 08:01:54 +0530

    Add RemoveElement.java

---
+++ LeetCode/RemoveElement.java
@@ -0,0 +1,8 @@
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) { int idx = 0;
+        for (int x : nums) if (x != val) nums[idx++] = x;
+        return idx;
+    };
+}
