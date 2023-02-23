7b380a0ab3a985431cbc1e41bd1fa49d68f9b4eb
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-02-23 05:40:59 +0530

    Add FindDuplicateNumber.java

---
+++ Arrays 1D/FindDuplicateNumber.java
@@ -0,0 +1,12 @@
+public class FindDuplicateNumber {
+
+    public int findDuplicate(int[] a) { int slow = a[0], fast = a[0];
+        do { slow = a[slow];
+            fast = a[a[fast]];
+        } while (slow != fast);
+        slow = a[0];
+        while (slow != fast) { slow = a[slow];
+            fast = a[fast];
+        } return slow;
+    };
+}
