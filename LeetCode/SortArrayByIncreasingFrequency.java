17ecb03605b5c6650a715e86f3859bd65d11a510
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-04-28 07:20:51 +0530

    Add SortArrayByIncreasingFrequency.java

---
+++ LeetCode/SortArrayByIncreasingFrequency.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class SortArrayByIncreasingFrequency {
+
+    public int[] frequencySort(int[] nums) { Map<Integer, Integer> mp = new HashMap<>();
+        for (int n : nums) mp.put(n, mp.getOrDefault(n, 0) + 1);
+        Integer[] a = new Integer[nums.length];
+        for (int i = 0;
+        i < nums.length;
+        i++) a[i] = nums[i];
+        Arrays.sort(a, (x, y) -> !mp.get(x).equals(mp.get(y)) ? Integer.compare(mp.get(x), mp.get(y)) : Integer.compare(y, x));
+        for (int i = 0;
+        i < nums.length;
+        i++) nums[i] = a[i];
+        return nums;
+    };
+}
