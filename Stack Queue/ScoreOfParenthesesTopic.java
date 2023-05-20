f4b8b7d2abb536125b13a52a18d4ff35a9d4dee3
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-05-20 16:35:24 +0530

    Add ScoreOfParenthesesTopic.java

---
+++ Stack Queue/ScoreOfParenthesesTopic.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ScoreOfParenthesesTopic {
+
+    public int scoreOfParentheses(String s) { Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for (char c : s.toCharArray()) { if (c == '(') st.push(0);
+            else { int v = st.pop(), w = st.pop();
+                st.push(w + Math.max(2 * v, 1));
+            } } return st.pop();
+    };
+}
