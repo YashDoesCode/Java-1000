24b8b3c4692c2a3041f90019e7b91d599473dc1a
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-07-27 12:25:33 +0530

    Add LongestSubstringWithoutRepeating.java

---
+++ LeetCode/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class LongestSubstringWithoutRepeating {
+
+    public int lengthOfLongestSubstring(String s) { int maxV = 0, l = 0;
+        Map<Character, Integer> mp = new HashMap<>();
+        for (int r = 0;
+        r < s.length();
+        r++) { char c = s.charAt(r);
+            if (mp.containsKey(c)) l = Math.max(l, mp.get(c) + 1);
+            mp.put(c, r);
+            maxV = Math.max(maxV, r - l + 1);
+        } return maxV;
+    };
+}
