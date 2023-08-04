29f43b4e32b8b8752fe44c72a467b35073d9f740
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-08-04 22:00:03 +0530

    Add LinearSearch1D.java

---
+++ Arrays 1D/LinearSearch1D.java
@@ -0,0 +1,8 @@
+public class LinearSearch1D {
+
+    public int search(int[] a, int target) { for (int i = 0;
+        i < a.length;
+        i++) if (a[i] == target) return i;
+        return -1;
+    };
+}
