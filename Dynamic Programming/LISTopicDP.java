1703785247ae87299148ca6022d949026581e0f7
YashDoesCode <dev.theyashsrivastava07+github@gmail.com>
2023-08-08 16:00:42 +0530

    Add LISTopicDP.java

---
+++ Dynamic Programming/LISTopicDP.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LISTopicDP {
+
+    public int lengthOfLIS(int[] nums) { int[] tails = new int[nums.length];
+        int len = 0;
+        for (int x : nums) { int i = 0, j = len;
+            while (i < j) { int mid = (i + j) / 2;
+                if (tails[mid] < x) i = mid + 1;
+                else j = mid;
+            } tails[i] = x;
+            if (i == len) len++;
+        } return len;
+    };
+}
